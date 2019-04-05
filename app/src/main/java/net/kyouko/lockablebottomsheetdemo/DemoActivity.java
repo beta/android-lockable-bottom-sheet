0a8a9effcfe5886df4c5c5820de1c6f0cc0e59a4
anmol-5732 <anmol.s@zohocorp.com>
2019-04-05 16:56:24 +0530

    Updating the gradle allowing event's to pass when touched outside and onBackpress closes the BottomMenu

--- app/src/main/java/net/kyouko/lockablebottomsheetdemo/DemoActivity.java
+++ app/src/main/java/net/kyouko/lockablebottomsheetdemo/DemoActivity.java
@@ -2,8 +2,10 @@ package net.kyouko.lockablebottomsheetdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class DemoActivity extends AppCompatActivity {
 
@@ -22,4 +24,11 @@ public class DemoActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Toast.makeText(this, "Hello from activity", Toast.LENGTH_SHORT).show();
+        }
+        return super.onTouchEvent(event);
+    }
 }
